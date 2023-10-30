754a0c48c9e090f0dc1022d874ed7a1b2a2c6dfd
ellen <ellen_yu@pixnet.tw>
2023-10-30 10:57:30 +0800

    feat: 新增SideBar元件

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-function App() {
+import SideBar from "./components/SideBar";
+
+const App = () => {
     return (
-        <>
+        <div className="flex">
+            <SideBar />
             <h1 className="my-8 text-center text-5xl font-bold">Hello World
                 <span>happy helloween</span>
             </h1>
-        </>
+        </div>
     );
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,8 @@
+
+const SideBar = () => {
+  return (
+    <div className="w-72 bg-black">SideBar</div>
+  )
+}
+
+export default SideBar;
