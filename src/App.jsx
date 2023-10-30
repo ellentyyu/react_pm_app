cf4c59f9d658f02ac999a2c10d43b53c74f824dd
ellen <ellen_yu@pixnet.tw>
2023-10-30 11:31:13 +0800

    feat: 調整sidebar樣式

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import SideBar from "./components/SideBar";
 
 const App = () => {
     return (
-        <div className="flex">
+        <div className="flex h-screen bg-neutral-100">
             <SideBar />
             <h1 className="my-8 text-center text-5xl font-bold">Hello World
                 <span>happy helloween</span>
             </h1>
         </div>
-    );
+);
 }
 
 export default App;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,11 @@
 
 const SideBar = () => {
-  return (
-    <div className="w-72 bg-black">SideBar</div>
-  )
+    return (
+        <div className="w-80 mt-10 pt-20 px-6 rounded-tr-xl bg-black text-neutral-100">
+            <h2 className="text-2xl text-neutral-100 font-semibold">YOUR PROJECTS</h2>
+            <button type="button" className="p-4 rounded-md bg-stone-700 text-stone-400">+ Add Project</button>
+        </div>
+    )
 }
 
 export default SideBar;
